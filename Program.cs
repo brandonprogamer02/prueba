80f9047c22a395592362b199dc1e79a47a3a30de
juniorulloa23 <59671640+juniorulloa23@users.noreply.github.com>
2021-07-07 00:28:08 -0400

    second commit
    Hola mundo modificado

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace tarea6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("HOLA MUNDO MODIFICADO!");
         }
     }
 }
